4fcb6901da52d22b89f46f2479d2f0e58fb06c72
Amy <yambeddm@gmail.com>
2024-04-03 08:04:04 +0200

    awake conversation started

---
+++ telegrambot/awake_conv.py
@@ -0,0 +1,164 @@
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+    CallbackContext
+)
+import os
+from typing import Final
+import psycopg2
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+BOT_USERNAME: Final = os.getenv('Bot')
+my_chat_id = os.getenv('my_chat_id')
+
+DB_CONFIG = {
+    'dbname': os.getenv('PG_DBNAME'),
+    'user': os.getenv('PG_USER'),
+    'password': os.getenv('PG_PASSWORD'),
+    'host': os.getenv('PG_HOST'),
+    'port': os.getenv('PG_PORT')
+}
+conn = psycopg2.connect(**DB_CONFIG)
+cursor = conn.cursor()
+
+# Only reply to messages from my chat_id
+def echo(update: Update, context: CallbackContext) -> None:
+    if update.message.chat_id == my_chat_id:
+        update.message.reply_text(update.message.text)    
+
+# Database gestion
+def create_table_if_not_exists(table_name, data):
+    create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ({data})"
+    cursor.execute(create_table_query)
+    conn.commit()
+def insert_data(table_name, data):
+    insert_query = f"INSERT INTO {table_name} VALUES %s"
+    cursor.execute(insert_query, (data,))
+    conn.commit()
+
+
+WAKE_UP, ASLEEP_TIME, LOCATION, BIO = range(4)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    reply_keyboard = [["Yes","No"]]
+
+    await update.message.reply_text(
+        "Hello Sunshine"
+        "Send /cancel to stop talking to me.\n\n"
+        "Did you wake up by yourself ?",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Yes or No?"
+        ),
+    )
+
+    return WAKE_UP
+
+
+async def asleep_time(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user = update.message.from_user
+    await update.message.reply_text(
+        "When did you fall asleep ?",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ASLEEP_TIME
+
+
+async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the photo and asks for a location."""
+    user = update.message.from_user
+    photo_file = await update.message.photo[-1].get_file()
+    await photo_file.download_to_drive("user_photo.jpg")
+    await update.message.reply_text(
+        "Gorgeous! Now, send me your location please, or send /skip if you don't want to."
+    )
+
+    return LOCATION
+
+
+async def skip_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Skips the photo and asks for a location."""
+    user = update.message.from_user
+    await update.message.reply_text(
+        "I bet you look great! Now, send me your location please, or send /skip."
+    )
+
+    return LOCATION
+
+
+async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the location and asks for some info about the user."""
+    user = update.message.from_user
+    user_location = update.message.location
+    await update.message.reply_text(
+        "Maybe I can visit you sometime! At last, tell me something about yourself."
+    )
+
+    return BIO
+
+
+async def skip_location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Skips the location and asks for info about the user."""
+    user = update.message.from_user
+    await update.message.reply_text(
+        "You seem a bit paranoid! At last, tell me something about yourself."
+    )
+
+    return BIO
+
+
+async def bio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    user = update.message.from_user
+    await update.message.reply_text("Ok babe, see you later and have a good day !")
+
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token("TOKEN").build()
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("/awake", start)],
+        states={
+            WAKE_UP: [MessageHandler(filters.Regex("^(Yes|No)$"), asleep_time)],
+            ASLEEP_TIME: [MessageHandler(filters.TEXT, photo), CommandHandler("skip", skip_photo)],
+            LOCATION: [
+                MessageHandler(filters.LOCATION, location),
+                CommandHandler("skip", skip_location),
+            ],
+            BIO: [MessageHandler(filters.TEXT & ~filters.COMMAND, bio)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+
+    application.add_handler(conv_handler)
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
